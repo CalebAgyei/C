49623a7c4b3c9e4a2a26501736f2d18c00c65ab1
CalebAgyei <calebnagyei@gmail.com>
2022-09-09 00:45:01 +0000

    My C tutorials

---
+++ io_demo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char f,m,l;
+	int age;
+	
+	printf("Please enter your initials, followed by your age: ");
+	scanf("%c %c %c %d", &f, &m, &l, &age);
+	printf("My initials are: %c%c%c and my age is %d.\n", f, m, l, age);
+	printf("My initials are: %c%c%c and my age is %d.\n", 'C', 'N', 'A', 31);
+
+	return (0);
+}
